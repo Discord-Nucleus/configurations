06221a2919d3dc2505c5cc10086f80ad597be64c
Käse Fuchs <kasefuchs@protonmail.com>
2022-11-23 19:51:02 +0300

    ♻️ Refactor prettier configuration
    * Content of `.prettierc` moved to `prettier.config.js`

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSameLine: true,
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  printWidth: 120,
+  quoteProps: "as-needed",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+};
